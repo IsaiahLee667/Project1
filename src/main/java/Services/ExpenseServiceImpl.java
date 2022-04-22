391b68c421f3cc2ce3ace7a381622fffb4c7f4f9
IsaiahLee667 <40584744+IsaiahLee667@users.noreply.github.com>
2022-04-22 10:31:52 -0400

    Added Update functionality, still doesn't work fully in postman Added log4j to the Pom

--- src/main/java/Services/ExpenseServiceImpl.java
+++ src/main/java/Services/ExpenseServiceImpl.java
@@ -20,7 +20,30 @@ public class ExpenseServiceImpl implements  ExpenseService{
     }
 
     @Override
+    public List<Expense> showAllExpenses() {
+        return this.expenseDAO.getAllExpenses();
+    }
+
+    @Override
     public List<Expense> findExpensesByAnEmployee(int empId) {
-        return null;
+        return this.expenseDAO.getExpensesByEmployeeId(empId);
+    }
+
+    @Override
+    public List<Expense> findExpensesByStatus(String status) {
+        return this.expenseDAO.getExpenseByStatus(status);
+    }
+
+    @Override
+    public Expense reviseExpense(Expense expense) throws IllegalAccessException {
+        if (expense.getStatus() == "Approved" || expense.getStatus() == "Denied"){
+            throw new IllegalAccessException("You cannot edit a transaction that is already Approved or Denied");
+        }
+        return this.expenseDAO.updateExpense(expense);
+    }
+
+    @Override
+    public Boolean removeExpenseById(int id) {
+        return expenseDAO.deleteExpenseById(id);
     }
 }
